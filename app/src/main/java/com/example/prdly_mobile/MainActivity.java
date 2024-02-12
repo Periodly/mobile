d5a59b5ca0c77a324af027f4b61ba7523650fc11
matt-pasek <mpasek@tm1.edu.pl>
2024-02-12 09:48:58 +0100

    hardcoded login logic

--- app/src/main/java/com/example/prdly_mobile/MainActivity.java
+++ app/src/main/java/com/example/prdly_mobile/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.prdly_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("loginState", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            // User is logged in, redirect to the next activity
+        }
+
+
+            Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener((view -> {
+            login();
+        }));
+
+    }
+
+    private void login() {
+        EditText usernameEditText = findViewById(R.id.login);
+        EditText passwordEditText = findViewById(R.id.password);
+
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (username.equals("admin") && password.equals("admin")) {
+            // set persistent state to true
+            SharedPreferences sharedPreferences = getSharedPreferences("loginState", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isLoggedIn", true);
+            editor.apply();
+
+            redirect();
+
+        } else {
+            TextView errorTextView = findViewById(R.id.loginError);
+            errorTextView.setText("Invalid username or password");
+        }
+    }
+
+    private void redirect() {
+        Intent intent = new Intent(MainActivity.this, AppHomeScreen.class);
+        startActivity(intent);
+        finish();
     }
 }
